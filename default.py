9ca3a1b5fca948f7fb0def0dd037e71e69c5afee
Rouzax <GitHub@mgdn.nl>
2026-04-08 08:26:38 +0000

    feat: add show and remove_keymap actions to default.py

--- default.py
+++ default.py
@@ -7,7 +7,46 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     action = args[0] if args else ""
 
-    if action == "test_overlay":
+    if action == "show":
+        # Lightweight signal to running service - no resources.lib imports
+        # to keep cold-start cost minimal (~50-100ms).
+        import time
+        import xbmcgui
+        xbmcgui.Window(10000).setProperty(
+            "chapternotify.trigger",
+            str(int(time.time() * 1000))
+        )
+
+    elif action == "remove_keymap":
+        from resources.lib import keymap
+        from resources.lib import log
+        log.init()
+        import xbmcgui
+        if keymap.is_installed():
+            ok = keymap.remove()
+            if ok:
+                xbmcgui.Dialog().notification(
+                    "Chapter Notify",
+                    "Keymap binding removed",
+                    xbmcgui.NOTIFICATION_INFO,
+                    3000,
+                )
+            else:
+                xbmcgui.Dialog().notification(
+                    "Chapter Notify",
+                    "Failed to remove keymap (see log)",
+                    xbmcgui.NOTIFICATION_ERROR,
+                    3000,
+                )
+        else:
+            xbmcgui.Dialog().notification(
+                "Chapter Notify",
+                "No keymap binding to remove",
+                xbmcgui.NOTIFICATION_INFO,
+                3000,
+            )
+
+    elif action == "test_overlay":
         from resources.lib.chapters import parse_chapter_name
         from resources.lib.overlay import create_chapter_overlay
 
